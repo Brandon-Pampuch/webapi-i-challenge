85330222e5694fedd480325a4d2d1e77ff8acfd4
Brandon-Pampuch <brandonpampuch@gmail.com>
2019-05-27 22:11:52 -0500

    add get users to server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express'); // import the express package
+const server = express(); // creates the server
+const db = require("./data/db")
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+server.get('/api/users', (req, res) => {
+    db.find().then(users =>{
+
+        res.status(200).json(users)
+    }).catch(err =>{
+        res.status(500).json({message:"error retrieving users"})
+    })
+});
+
+// watch for connections on port 5000
+server.listen(4000, () =>
+  console.log('Server running on http://localhost:5000')
+);
+
